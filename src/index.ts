154f3eb9f96168558651c0321f278b148e4032db
Arnaud Lewis <arnaud.lewis@gmail.com>
2017-08-01 16:21:29 +0200

    add richtext helper

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import Serialize from '@root/serialize';
 import { ElementKindAsObj } from '@root/elements';
 
 module.exports = {
+  asText: (richtext) => {
+    return richtext.reduce((acc, block) => {
+      return `${acc} ${block.text}`;
+    }, '\n');
+  },
   asTree: Tree.fromRichText,
   serialize: Serialize,
   Elements: ElementKindAsObj
