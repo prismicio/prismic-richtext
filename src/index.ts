dac153b5c3395b46168c272d4e2a474b79998d4d
Nick Zahn <nick@cloudunder.io>
2018-02-21 11:33:36 +0000

    Rewrite of the asText function and basic tests

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
+import asText from "./astext";
 import Tree from "./tree";
 import Serialize from "./serialize";
 
 module.exports = {
-  asText: (richtext: any) => {
-    return richtext.reduce((acc: any, block: any) => {
-      return `${acc} ${block.text}`;
-    }, '\n');
-  },
+  asText,
   asTree: Tree.fromRichText,
   serialize: Serialize,
   Elements: Tree.NODE_TYPES,
