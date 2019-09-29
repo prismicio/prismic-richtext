2997fe7f14a542365dc65fc5d8cadbe71b739719
Harrison Ifeanyichukwu <Harrisonifeanyichukwu@gmail.com>
2019-09-29 11:39:38 +0200

    ci: update build process, use semantic release

--- d.ts/serialize.d.ts
+++
@@ -1,2 +0,0 @@
-declare function fromRichText<T>(richText: any[], serialize: (type: string, data: any, text: string | null, children: T[] | null) => T, htmlSerializer: (data: any, text: string | null, children: T[] | null) => T): T[];
-export default fromRichText;
