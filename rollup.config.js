34c22dd575480c2ce851d795426103c1de5e806e
Bogdan Chadkin <trysound@yandex.ru>
2020-08-08 15:01:33 +0300

    Replace webpack with rollup
    - rollup does not include 1kb runtime
    - rollup generates more compact code
    - rollup is able to output es modules
    
    Before
    
    33542 prismic-richtext.js
    12988 prismic-richtext.min.js
    
    After
    22581 prismic-richtext.esm.js
    24374 prismic-richtext.js
    10259 prismic-richtext.min.js
    
    I also added "module" field for bundlers. "main" refers to not minified
    code to simplify debugging.

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { babel } from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+
+const name = "PrismicRichtext";
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      file: './dist/prismic-richtext.esm.js',
+      format: 'esm'
+    },
+    {
+      file: './dist/prismic-richtext.js',
+      format: 'umd',
+      name
+    },
+    {
+      file: './dist/prismic-richtext.min.js',
+      format: 'umd',
+      name,
+      plugins: [terser()]
+    }
+  ],
+  plugins: [
+    nodeResolve({
+      extensions: ['.ts']
+    }),
+    babel({
+      extensions: ['.ts'],
+      babelHelpers: 'bundled',
+      presets: [
+        "@babel/typescript",
+        ["@babel/env", {
+          // transpile class properties
+          shippedProposals: true,
+        }],
+      ]
+    }),
+  ]
+};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import asText from "./astext";
 import Tree from "./tree";
-import Serialize from "./serialize";
 import { NODE_TYPES } from "./types";
 
-module.exports = {
-  asText,
-  asTree: Tree.fromRichText,
-  serialize: Serialize,
-  Elements: NODE_TYPES,
-};
+export { default as asText } from "./astext";
+export { default as  serialize } from "./serialize";
+
+export const asTree = Tree.fromRichText;
+export const Elements = NODE_TYPES;
