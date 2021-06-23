8937a2463f526e3231e5fa4e5d6d15f2b325eb55
lihbr <lihbr@users.noreply.github.com>
2021-06-23 18:07:36 +0200

    test: update asTree test to make sure field doesn't mutate

--- test/asTree.test.ts
+++ test/asTree.test.ts
@@ -4,6 +4,15 @@ import { richTextFixture } from "./__fixtures__/richText";
 
 import { asTree } from "../src";
 
+// Keep this test first so that we're sure it's the first one to access the fixture
+test("does not mutate the provided rich text field", (t) => {
+	const richTextJSON = JSON.stringify(richTextFixture.en);
+
+	asTree(richTextFixture.en);
+
+	t.deepEqual(JSON.parse(richTextJSON), richTextFixture.en);
+});
+
 test("converts a rich text field value to a tree", (t) => {
 	t.snapshot(asTree(richTextFixture.en));
 });
